b801783cf9d350e323c730623ec0adecae477f4a
Andrew Thomason <andrewt77@gmail.com>
2013-02-12 22:36:37 -0600

    fixed sorting problem, time to go to 1.3

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -2,15 +2,20 @@ class DealsController < ApplicationController
   # GET /specials
   # GET /specials.json
   helper_method :sort_column, :sort_direction
+  
+  
   def index
-     @specials = Special.order(sort_column + ' ' + sort_direction).where("day = ?", Time.now.wday)
+     @specials = Special.order.where("day = ?", Time.now.wday)
 
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @specials }
 	end
   end
- 
+  
+  def show
+    @upcoming = Special.order(sort_column + ' ' + sort_direction).where("day >= ?", Time.now.wday)
+  end 
   private
   def sort_column
     Special.column_names.include?(params[:sort]) ? params[:sort] : "bar_id"
